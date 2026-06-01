e3564fd152e335800faea6779b985546ed8af756
Fidzz-Hub <hafidzsfsmd22@gmail.com>
2026-06-01 22:26:26 +0700

    Landing Pages Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
-import LoginForm from "./LoginForm";
-import SignUpForm from "./SignUpForm";
+// import LoginForm from "./LoginForm";
+// import SignUpForm from "./SignUpForm";
+
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Service";
+
+// import React from 'react';
+
 
 function App() {
   return (
-    <div className="min-h-screen flex bg-[#6E95FF]">
+    // <div className="min-h-screen flex bg-[#6E95FF]">
 
-      {/* Left */}
-      <div className="hidden md:flex flex-1 flex-col justify-center items-center text-white px-10">
-        <h1 className="text-5xl font-bold mb-3">
-          WELCOME TO NODETEAMM
-        </h1>
+    //   {/* Left */}
+    //   <div className="hidden md:flex flex-1 flex-col justify-center items-center text-white px-10">
+    //     <h1 className="text-5xl font-bold mb-3">
+    //       WELCOME TO NODETEAMM
+    //     </h1>
 
-        <p className="text-lg opacity-90">
-          Login dan daftar dengan mudah
-        </p>
-      </div>
+    //     <p className="text-lg opacity-90">
+    //       Login dan daftar dengan mudah
+    //     </p>
+    //   </div>
 
-      {/* Right */}
-      <div className="w-full md:w-[45%] bg-white md:rounded-l-[60px] rounded-t-[50px] md:rounded-t-none flex items-center justify-center p-8">
+    //   {/* Right */}
+    //   <div className="w-full md:w-[45%] bg-white md:rounded-l-[60px] rounded-t-[50px] md:rounded-t-none flex items-center justify-center p-8">
 
-        <div className="w-full max-w-md space-y-8">
-          <LoginForm />
-          <SignUpForm />
-        </div>
+    //     <div className="w-full max-w-md space-y-8">
+    //       <LoginForm />
+    //       <SignUpForm />
+    //     </div>
 
-      </div>
+    //   </div>
+    // </div>
+    <div className="min-h-screen bg-white text-slate-900">
+      <Navbar />
+      <Hero />
+      <Services />
+      <Features />
+      <Footer />
     </div>
+
   );
 }
 
